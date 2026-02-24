b2323947891be0fd03327f395bf33f5dcea85809
Shoh1202 <shohruhkas@gmail.com>
2026-02-24 19:11:24 +0300

    Delete NEW directory

--- NEW/news/my_filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import FilterSet
-from django import forms
-from .models import Post
-class PostFilter(FilterSet):
-   class Meta:
-       model=Post
-       fields={'title':['icontains'],
-           'author':['exact'],
-               'time_add':['gt']}
